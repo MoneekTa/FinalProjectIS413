ee6a03faf809307400db67f04a9573cff36c2326
Moneek <mot1@umbc.edu>
2023-07-03 00:11:28 -0400

    First Push

--- src/main/java/com/denisemoneek/finalproject/Game.java
+++ src/main/java/com/denisemoneek/finalproject/Game.java
@@ -1,19 +1,41 @@
 package com.denisemoneek.finalproject;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Game extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Pane gamePane = new Pane();
+import java.io.FileNotFoundException;
 
-        Scene scene = new Scene(gamePane, 800, 600);
+public class Game extends Application {
+    Layout layout = new Layout();
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException {
+
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        Scene scene = new Scene(pane,400,400);
+        scene.setFill(layout.Color());
+        //int status = (int) (Math.random()*3);
+        //if (status == 1) {
+        //    scene.setFill(layout.Color());
+        //} else {
+            //Image backgroundImage = layout.Image().getImage();
+            //pane.setStyle("-fx-background-image: url('" + backgroundImage.getUrl() + "'); " +
+            //        "-fx-background-size: cover;");
+
+        //};
+        primaryStage.setTitle("LayoutTest");
         primaryStage.setScene(scene);
-        primaryStage.setTitle("JFX Invaders");
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
