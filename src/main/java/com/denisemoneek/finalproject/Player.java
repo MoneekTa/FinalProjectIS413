01566a7eb9c3e38e086d7fdcac73fb8aa086b010
Denise-1 <denisechec@gmail.com>
2023-07-03 13:28:58 -0400

    player class and PlayerAesthetic added

---
+++ src/main/java/com/denisemoneek/finalproject/Player.java
@@ -0,0 +1,4 @@
+package com.denisemoneek.finalproject;
+
+public class Player {
+}
---
+++ src/main/java/com/denisemoneek/finalproject/PlayerAesthetic.java
@@ -0,0 +1,7 @@
+package com.denisemoneek.finalproject;
+
+import javafx.scene.shape.Shape;
+
+public interface PlayerAesthetic {
+    Shape createPlayerShape();
+}
