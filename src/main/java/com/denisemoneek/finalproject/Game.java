1cc86ab54b465e58a3a04a7861c5291a81609361
Moneek <mot1@umbc.edu>
2023-07-03 10:42:40 -0400

    First Push

--- src/main/java/com/denisemoneek/finalproject/Game.java
+++ src/main/java/com/denisemoneek/finalproject/Game.java
@@ -1,14 +1,39 @@
 package com.denisemoneek.finalproject;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Game extends Application {
+    long elaspedtime;
+    Timer timer = new Timer();
+    Boolean timerRun = false;
     @Override
     public void start(Stage primaryStage) {
-        Pane gamePane = new Pane();
+
+        Button PauseButton = new Button("Start Game");
+        Pane gamePane = new Pane(PauseButton);
+        timer.start();
+        PauseButton.setOnAction(event -> {
+            //uses this print code to debug
+            System.out.println("Game Started");
+            //if the timerRun is false, meaning the timer is not running
+            if(!timerRun){
+                timer.start();
+                PauseButton.setText("Game is Running");
+            } else {
+                elaspedtime = elaspedtime + timer.stop();
+                PauseButton.setText("Time: " + elaspedtime + " seconds");
+            }
+            //resets timerRun
+            timerRun = !timerRun;
+        });
 
         Scene scene = new Scene(gamePane, 800, 600);
         primaryStage.setScene(scene);
--- src/main/java/com/denisemoneek/finalproject/Timer.java
+++ src/main/java/com/denisemoneek/finalproject/Timer.java
@@ -11,15 +11,17 @@ public class Timer {
     long time;
     long elaspedTime;
 
-    public void start(int time){
-        starttime = time + System.currentTimeMillis();
+    public void start(){
+        starttime =  System.currentTimeMillis();
+        System.out.println(starttime/1000);
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000)));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
     }
     public long stop(){
-        endtime = System.currentTimeMillis();
-        elaspedTime = endtime - starttime;
-        return elaspedTime/1000;
+        endtime = System.currentTimeMillis() ;
+        System.out.println(endtime/1000);
+        elaspedTime = endtime- starttime;
+        return elaspedTime;
     }
 }
