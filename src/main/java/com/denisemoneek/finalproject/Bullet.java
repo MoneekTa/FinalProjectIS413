6fd6c205866f6481d9a0a84ff621c99e10640cf5
Denise Chen <78581408+Denise-1@users.noreply.github.com>
2023-07-11 22:58:42 -0400

    Update Bullet.java

--- src/main/java/com/denisemoneek/finalproject/Bullet.java
+++ src/main/java/com/denisemoneek/finalproject/Bullet.java
@@ -1,9 +1,15 @@
 package com.denisemoneek.finalproject;
 
 public interface Bullet {
+
+    // movement of bullet
     public void moveBullet();
+
+    // where the bullet will be 
     public void setYPosition(double yPosition);
     public void setXposition(double xPosition);
+
+    // where the bullet will end at 
     public long recordY();
     public long recordX();
 
